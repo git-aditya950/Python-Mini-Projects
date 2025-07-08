466cc721be1228cf226e375b793e609578189b9b
Aditya <ceaser.adi00@gmail.com>
2025-07-08 10:32:50 +0530

    Update qr_generator.py

--- qr_generator.py
+++ qr_generator.py
@@ -14,13 +14,10 @@ def generate_qr(data, filename):
     img = qr.make_image(fill_color="black", back_color="white")
     
     img.save(filename)
-    print(f"✅ QR Code saved as {filename}")
+    print(f"QR Code saved as {filename}")
 
-print("🔲 QR Code Generator")
+print("QR Code Generator")
 data = input("Enter the data to encode (URL, text, etc): ")
 filename = input("Enter filename to save (e.g., myqr.png): ")
 
 generate_qr(data, filename)
-
-
-
